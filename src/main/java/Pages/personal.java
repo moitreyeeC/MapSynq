3e0a1b5be7c4316284791adc863648fffb6b2a5f
Moitreyee <moitreyee.jak@gmail.com>
2019-02-13 11:01:32 +0530

    fresh commit

--- src/main/java/Pages/personal.java
+++ src/main/java/Pages/personal.java
@@ -9,8 +9,7 @@ import Utility.DriverBuilder;
 public class personal extends DriverBuilder {
 @FindBy(xpath="//a[@class='tab_button me_tab sprite']")WebElement personal_tab;
 @FindBy(xpath="//input[@value='Register']")WebElement register_button;
-@FindBy(xpath="//input[@value='Register']")WebElement register_button;
-@FindBy(xpath="//input[@value='Register']")WebElement register_button;
+
 
 	public personal(WebDriver driver) {
 		super(driver);
